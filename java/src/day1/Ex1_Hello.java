2a98c9ac77f0328ce05173b32c33a6583893758c
hyejin960622 <ch5335@gmail.com>
2022-04-25 13:57:23 +0900

    자바 1일차 Hello world 출력 예제

---
+++ java/src/day1/Ex1_Hello.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Ex1_Hello {
+// 콘솔 응용 프로그램이 실행되기 위해서 main이라는 메소드가 필요하다.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* println(문자열) : 문자열을 콘솔에 출력한 후 줄바꿈을 함. 
+		 * print(문자열) 문자열을 콘솔에 출력
+		 * */
+		 System.out.println("Hello world!!"); 
+}
+
+}
