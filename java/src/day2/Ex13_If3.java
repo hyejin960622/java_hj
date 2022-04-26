5d9136e9705672be8f224f09cef89d63e53bd5d6
hyejin960622 <ch5335@gmail.com>
2022-04-26 17:01:58 +0900

    자바2일차 : else if 문 예제

---
+++ java/src/day2/Ex13_If3.java
@@ -0,0 +1,31 @@
+package day2;
+
+public class Ex13_If3 {
+
+	public static void main(String[] args) {
+		/* num가 양수이면 양수라고 출력하고
+		 * num가 음수이면 음수라고 출력하고
+		 * num가 0이면 0이라고 출력하는 코드 if문을 이용하여 작성하세요 
+		 * */
+		/* if문에서 
+		 * if는 반드시 1개 있어야 함.
+		 * else if는 0개 이상 있을수 있다.
+		 * else는 0개 또는 1개
+		 * 
+		 * */
+		
+		int num = 7;
+		
+		if(num > 0) {
+			System.out.println(num + "는 양수입니다.");
+		} 
+		else if(num < 0) {
+			System.out.println(num + "는 음수입니다.");
+		}
+		else {
+			System.out.println(num + "는 0입니다");
+		}
+
+	}
+
+}
