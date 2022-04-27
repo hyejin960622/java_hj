9b1a3e76fcb4c20522aab5e710175f8d030ce0f2
hyejin960622 <ch5335@gmail.com>
2022-04-27 22:45:19 +0900

    자바 3일차 : for 최대공약수 예제

---
+++ java/src/day3/Ex16_For6_GCD.java
@@ -0,0 +1,67 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Ex16_For6_GCD {
+
+	public static void main(String[] args) {
+		/* 두 정수의 최대 공약수를 구하는 코드를 작성하세요.
+		 * 공약수 : 공통으로 있는 약수 
+		 * 최대공약수 : 공약수중 가장 큰 수 
+		 * 12 : 1 2 3 4 6 12
+		 * 18 : 1 2 3 6 9 18
+		 * 12와 18의 공약수 : 1 2 3 6
+		 * 12와 18의 최대공약수 : 6
+		 * 반복횟수 : i는 1부터 num1까지 1씩증가
+		 * 규칙성 : i가 num1의 약수이고 i가 num2의 약수이면 i를 gcd라는 변수에 저장
+		 * => num1을 i로 나누었을 때 나머지가 0과 같고 num2를 i로 나누었을때 나머지가 0과 같다면 i를 gcd에 저장
+		 * 반복문 종료 후 : gcd를 출력
+		 * */
+		
+		/* 같은 타입의 변수를 여러개 선언하는 경우 ,를 통해서 한줄로 선언할 수 있다.
+		 * 타입변수명1; 
+		 * 타입변수명2
+		 * 타입 변수명3, 변수명4;*/
+		/*변수의 사용 범위
+		 * i는 반복문 밖에서 선언되어 반복문 이후에도 사용 가능
+		 * j는 반목에 안에 선언되어 반목문엑서만 사용가능*/
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("두 정수를 입력하세요. : ");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		
+		int max, min;
+		if (a>b) {
+			max = a;
+			min = b;
+		}
+		else {
+			max =b;
+			min =a;
+		}
+			
+		int gcd= 1;
+		int i =1;
+
+		for( i = 1 ; i <= min ; i++) {
+			if (max % i==0 && min % i==0) {
+				gcd = i;
+			}	
+		}
+		System.out.println("최대 공약수는" + gcd);
+	}
+		
+}
+
+		
+	
+
+
+
+		
+		
+		
+		
