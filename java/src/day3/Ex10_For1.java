d185b845ef86f93fbcbf2471360b137de3515497
hyejin960622 <ch5335@gmail.com>
2022-04-27 17:30:15 +0900

    자바 3일차 : for 규칙찾기 예제

---
+++ java/src/day3/Ex10_For1.java
@@ -0,0 +1,30 @@
+package day3;
+
+public class Ex10_For1 {
+
+	public static void main(String[] args) {
+		/* 콘솔에 Hello를 5번 출력하세요
+		 * 규칙성찾기
+		 * hello i =1 hello
+		 * hello i =2 hello
+		 * hello i =3 hello 
+		 * hello i =4 hello
+		 * hello i =5 hello
+		 * 
+		 * 반복횟수 : i는 1	부터 5까지 1식증가
+		 * 초기화 : i는 1부터
+		 * 조건식 : i는 5까지(초기값에 따라 까지가 의미가 달라진다)
+		 * 증감식 : i는 1씩 증가
+		 * 규칙성(실행문) : hello를 출력
+		 * */
+		
+		
+	    int i;
+		for(i=1 ; i<=5 ;i++) {
+			System.out.println("Hello");
+			
+		}
+		
+	}
+
+}
