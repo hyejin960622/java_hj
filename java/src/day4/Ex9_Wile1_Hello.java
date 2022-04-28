438708e5e1e4494ff650c7c753921ca8aedfc106
hyejin960622 <ch5335@gmail.com>
2022-04-28 17:00:53 +0900

    자바 4일차 : while문 - hello 예제

---
+++ java/src/day4/Ex9_Wile1_Hello.java
@@ -0,0 +1,22 @@
+package day4;
+
+public class Ex9_Wile1_Hello {
+
+	public static void main(String[] args) {
+		/*헬로우를 5번 출력하는 예제*/
+		int i = 1;
+		while(i<=5) {
+			System.out.println("Hello");
+			i++;
+		}
+		
+		i=5;
+		while(i-->0) {
+			System.out.println("hi");
+		}
+		
+		
+
+	}
+
+}
