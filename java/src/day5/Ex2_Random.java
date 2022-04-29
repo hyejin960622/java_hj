04cab08308565177143e75ebd163a60d22c9fd9a
hyejin960622 <ch5335@gmail.com>
2022-04-29 17:02:20 +0900

    자바 5일차 : 랜덤한 수 출력 예제

---
+++ java/src/day5/Ex2_Random.java
@@ -0,0 +1,24 @@
+package day5;
+
+public class Ex2_Random {
+
+	public static void main(String[] args) {
+		/* 0< = Math.random() < 1 실수 
+		 * 0 * (max-min+1)< = Math.random()*(max-min+1) < 1 * (max-min+1)
+		 * 0 < = Math.random()*(max-min+1) < (max-min+1)
+		 * 0 + min < = Math.random()*(max-min+1) + min < (max-min+1) + min
+		 * min <=  Math.random()*(max-min+1) + min < max + 1
+		 * */
+		 
+		
+		int min = 5, max = 10;
+		int r;
+		for(int i = 1; i <=10; i++) {
+			 r = (int)(Math.random() * (max-min+1)+ min); //int위치 전체에 줘야함. 잘못하면 최소값만 나옴.
+			 System.out.print(r +" ");
+		}
+		
+
+	}
+
+}
