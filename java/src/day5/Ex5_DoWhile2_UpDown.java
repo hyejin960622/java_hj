7adcaf74ed1f290147d0cffa4c0e505102d3bd77
hyejin960622 <ch5335@gmail.com>
2022-04-29 17:02:57 +0900

    자바 5일차 : do while문 예제 - 정수 Up Down 예제

---
+++ java/src/day5/Ex5_DoWhile2_UpDown.java
@@ -0,0 +1,62 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Ex5_DoWhile2_UpDown {
+
+	public static void main(String[] args) {
+		/* 랜덤으로 생성한 수(1~100)를 맞추는 게임을 작성하세요.
+		 * 정수를 입력했을 때 입력한 정수보다 랜더믕로 새엇ㅇ된 수가 크면 UP, 작으면 DOWN을 표시
+		 * 맞으면 맞췄습니다라고 표시
+		 * 랜덤으로 생성한 수 : 40
+		 * 입력 : 100 => DOWN
+		 * 입력 : 50 => DOWN
+		 * 입력 : 25 => UP
+		 * 입력 : 40 => 정답입니다.
+		 * 
+		 * 반복횟수 : 무한대
+		 * 규칙성 : 정수를 입력받고, 
+		 * 입력받은 정수가 랜덤한수  r과 같으면 정답입니다라고 출력한후 빠져나가고,
+		 * 입력받은 정수가 랜덤한수  r과 같지않고 r보다 크면 down이라고 출력
+		 * 입력받은 정수가 랜덤한수  r과 같지않고 r보다 작으면 up이라고 출력
+		 * */
+		
+		
+	
+		Scanner scan = new Scanner (System.in);
+		
+		int min = 1, max = 100;
+		int r = (int)(Math.random() * (max-min+1)+ min);
+		System.out.println("랜덤으로 생성한 수(1~100) : " + r);
+		int num;
+		
+		do{
+			System.out.println("1~100사이의 정수를 입력하세요 :");
+			num = scan.nextInt(); //정수를 입력받고,
+			//1미만 100초과인 경우 스킵
+			if(num < 1 || num >100) {
+				continue;
+			}
+		
+		//입력받은 정수가 랜덤한수가 r보다 크면 down이라고 출력
+		if(num > r) {
+			System.out.println("down");
+		}
+		//입력받은 정수가 랜덤한수가 r보다 작으면 up이라고 출력
+		else if(num < r){
+			System.out.println("up");
+		}
+	
+		}while(num != r);
+		System.out.println("정답입니다.");
+		scan.close();
+		
+		
+	}	
+	
+
+
+	
+	}
+
+
