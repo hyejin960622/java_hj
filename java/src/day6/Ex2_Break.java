2654e0c23a052935d63e8a9c24b42f22afb293b1
hyejin960622 <ch5335@gmail.com>
2022-05-02 17:20:08 +0900

    자바 6일차 : 중첩 반복문에서 break 예제

---
+++ java/src/day6/Ex2_Break.java
@@ -0,0 +1,30 @@
+package day6;
+
+public class Ex2_Break {
+
+	public static void main(String[] args) {
+		/**/
+		
+		int i, j;
+		for(i=1; i<=5; i++) {
+			for(j=1; j<=5; j++) {
+				System.out.println(i +"," + j);
+				if(j==3) {
+					break;
+				}
+			}
+			//break를 만나면 이 위치로 이동
+		}
+		System.out.println("---------------------------");
+		out:for(i=1; i<=5; i++) {
+			for(j=1; j<=5; j++) {
+				System.out.println(i +"," + j);
+				if(j==3) {
+					break out;
+				}
+			}
+
+	}
+
+}
+}
