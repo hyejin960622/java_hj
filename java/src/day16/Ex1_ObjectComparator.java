2279cc0a8b01bf4909279307c9a9ad1821989800
hyejin960622 <ch5335@gmail.com>
2022-05-17 17:12:06 +0900

    자바 16일차 : Objects 예제 - compare 예제

---
+++ java/src/day16/Ex1_ObjectComparator.java
@@ -0,0 +1,29 @@
+package day16;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Ex1_ObjectComparator {
+
+	public static void main(String[] args) {
+		Integer num1 = 10, num2 = 20;
+		/* Objects.compare()에서 세번째 매개변수는 Comparator 인터페이스의 객체가 필요
+		 * => Comparator 인터페이스의 구현 클래스가 필요
+		 * => 구현 클래스 대신 익명 객체를 이용
+		 * => Objects.compare()의 리턴값
+		 * 		=> 0 : 같다
+		 * 		=> 음수 : 앞에 수가 작다
+		 * 		=> 양수 : 앞에 수가 크다
+		 * */
+		System.out.println(Objects.compare(num2, num1, new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				return o1-o2;
+			}
+		}));
+
+	}
+
+}
