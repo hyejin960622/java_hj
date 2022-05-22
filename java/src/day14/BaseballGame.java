100125cf6e8d10277ae321c84dc8ba2fcf133f90
hyejin960622 <ch5335@gmail.com>
2022-05-22 15:36:13 +0900

    자바 18일차 : List를 이용한 숫자 야구 게임 프로그램 예제

--- java/src/day14/BaseballGame.java
+++ java/src/day14/BaseballGame.java
@@ -1,14 +1,18 @@
 package day14;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class BaseballGame implements ConsoleProgram{
 	private Scanner scan;
 	private final int exitMenu = 3;
-	private int com[] = new int[3];
-	private int user[] = new int[3];
-	private int record[] = new int[10];
-	private int recordCount = 0; //기록에 등록된 갯수
+	private List<Integer> com = new ArrayList<Integer>();
+	private List<Integer> user = new ArrayList<Integer>();
+	private List<Integer> record = new ArrayList<Integer>();
+	
 	private int min = 1;
 	private int max = 9;
 
@@ -32,14 +36,25 @@ public class BaseballGame implements ConsoleProgram{
 		case 1:
 			//컴퓨터 랜덤수 생성
 			createComRandom();
-			//반복
-				//사용자 숫자 입력
-				//S,B,O을 판별
-			//기록 저장 정렬?
+			System.out.println();
+			
+			int count = 0;
+			while(true) {
+				inputNumbers(3);
+				//횟수 1증가
+				count++;
+				//스트라이크, 볼 결과를 출력후, 게임 종료 여부를 알려줌
+				if(printResult()) {
+					break;
+				}
+			}
+			record(count,5);
 			break;
 		case 2:
+			printRecord();
 			break;
 		case 3:
+			
 			break;
 		default :
 			System.out.println("잘못된 메뉴를 선택했습니다");
@@ -61,17 +76,18 @@ public class BaseballGame implements ConsoleProgram{
 		
 	}
 	public void createComRandom() {
-		int count = 0;
-		while(count < com.length) {
+		com.clear(); //이전 플레이에서 사용한 숫자들을 지움.
+		while(com.size() <3) {
 			//랜덤 수 생성
 			int r = random(min, max);
 			//중복이 아니면저장하고 count를 증가
-			if(indexOf(com, count, r) == -1) {
-				com[count++] = r;
-				System.out.println(r + " ");
+			if(com.indexOf(r) == -1) {
+				com.add(r);
+				System.out.print(r + " ");
 			}
 		}
 	}
+	
 	public static int random(int min, int max) {
 		if(min > max) {
 			int tmp = min;
@@ -80,23 +96,108 @@ public class BaseballGame implements ConsoleProgram{
 		}
 		return (int)(Math.random()*(max - min +1) + min);
 	}
-	public static int indexOf(int arr[], int size, int num) {
-		if(size == 0) {
-			return -1;
-		}
-		if(arr.length < size) {
-			size = arr.length;
-		}
-		for(int i = 0; i<size; i++) {
-			if(arr[i] == num) {
-				return i;
-			}
-		}
-		return -1;
-	}
+	
 	
 	public BaseballGame(Scanner scan) {
 		this.scan = scan;
 	}
+	
+	public void inputNumbers(int count) {
+		user.clear();
+		String str = "중복되지 않게 {0}~{1}사이의 {2}개의 숫자를 입력하세요.";
+		String formatStr = MessageFormat.format(str, min, max, count);
+		System.out.println(formatStr);
+		System.out.println("입력 : ");
+		
+		
+		while(user.size() <count) {
+			try {
+				int num = scan.nextInt();
+				//범위 넘어가는 숫자이면 예외 발생
+				if(num < min || num > max) {
+					throw new ArithmeticException();
+				}
+				//중복된 숫자이면 예외 발생
+				if(user.contains(num)) {
+					throw new ArithmeticException();
+				}
+				user.add(scan.nextInt());				
+			}
+			//범위가 아닌 숫자를 입력했을 때, 중복될때
+			catch(ArithmeticException e) {
+				System.out.println("중복 되거나 범위를 벗어났습니다. 다시 입력하세요.");
+				if(scan.hasNext()) {
+					scan.nextLine();
+				}
+				System.out.println(formatStr);
+				System.out.println("입력 : ");
+				user.clear();
+			}
+			//숫자가 아닌 문자를 입력했을 때 
+			catch(Exception e) {
+				System.out.println("입력이 잘못되었습니다. 다시 입력하세요.");
+				scan.nextLine();
+				System.out.println(formatStr);
+				System.out.println("입력 : ");
+				user.clear();
+			}
+		}
+		
+	}
+	
+	public boolean printResult() {
+		//스트라이크, 볼 개수 확인
+		int strike = 0;
+		for(int i =0; i <com.size() ; i++) {
+			if(com.get(i).equals(user.get(i))) {
+				strike++;
+			}
+		}
+		int ball = 0;
+		for(Integer tmp : com) {
+			if(user.contains(tmp)) {
+				ball++;
+			}
+		}
+		ball = ball - strike;
+		//스트라이크, 볼 개수에 따라 결과 출력
+		if(strike != 0) {
+			System.out.println(strike + "S");
+		}
+		if(ball != 0) {
+			System.out.println(ball + "B");
+		}
+		if(strike == 0 && ball == 0) {
+			System.out.println("O");
+		}
+		System.out.println();
+		if(strike ==3) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void record(int count, int max) {
+		record.add(count);
+		record.sort(new Comparator<Integer>() {
 
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1 - o2;
+			}
+		});
+		if(record.size() > max) {
+			record.remove(max);
+		}
+	}
+	public void printRecord() {
+		if(record.size() == 0) {
+			System.out.println("기록이 없습니다. 도전하세요");
+			return;
+		}
+		for(int i = 0; i<record.size(); i++) {
+			System.out.println(i + 1 + "등." + record.get(i) + "회");
+		}
+		System.out.println(record);
+	}
 }
