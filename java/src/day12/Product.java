21fc95acb1ef2965e880e59312cebb4fddd407b1
hyejin960622 <ch5335@gmail.com>
2022-05-22 15:35:39 +0900

    자바 12일차 : 마트 관리 프로그램

--- java/src/day12/Product.java
+++ java/src/day12/Product.java
@@ -3,42 +3,37 @@ package day12;
 public abstract class Product {
 	//제품명
 	private String name;
-	
 	//가격
 	private int price;
-	
 	//수량
 	private int amount;
 	
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getPrice() {
 		return price;
 	}
-
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
-	
 	public int getAmount() {
 		return amount;
 	}
-
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
-
+	/* 기능 : 수량이 주어지면 기존 수량에 누적하는 메소드
+	 * */
+	public void sumAmount(int amount) {
+		this.amount += amount;
+	}
+	
 	public Product(String name) {
-		this(name,0,0);
-		
+		this(name, 0, 0);
 	}
 	public Product(String name, int price, int amount) {
 		this.name = name;
@@ -46,5 +41,4 @@ public abstract class Product {
 		this.amount = amount;
 	}
 	public abstract void print();
-
 }
