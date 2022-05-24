7e420104b1d54c85b9fc6d2209632f2797a41e59
hyejin960622 <ch5335@gmail.com>
2022-05-24 09:33:14 +0900

    자바 20일차 : 파일관리 프로그램 예제

---
+++ java/src/day20/Test.java
@@ -0,0 +1,73 @@
+package day20;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		Scanner scan = new Scanner(System.in);
+	
+		
+		int menu;
+		int exitMenu = 4;
+		String fileName;
+		
+		
+		
+		do {
+			System.out.println("메뉴");
+			System.out.println("1. 파일 저장");
+			System.out.println("2. 파일 확인");
+			System.out.println("3. 파일 검색");
+			System.out.println("4. 프로그램 종료");
+			System.out.print("메뉴를 선택하세요 : ");
+			
+			menu = scan.nextInt();
+			
+			switch(menu) {
+			case 1:
+				System.out.print("파일명 : ");
+				fileName = scan.next();
+				System.out.println("파일 저장이 완료되었습니다.");
+				list.add(fileName);
+				break;
+			case 2:
+				for(String tmp : list) {
+		               System.out.println(tmp + " ");
+		            }
+				break;
+			case 3:
+				System.out.print("검색어 : ");
+				fileName = scan.next();
+				
+				for(String tmp : list) {
+					for(int i = 0; i < list.size() ; i++) {
+						if(tmp.contains(fileName)) {
+						}
+						System.out.println(list.get(i));
+					}
+				}
+				break;
+				
+			case 4 :
+				System.out.println("프로그램을 종료합니다.");
+				
+				break;
+			default :
+				
+			}
+			
+		}while(menu != exitMenu);
+		
+		
+		
+		
+	}
+
+}
