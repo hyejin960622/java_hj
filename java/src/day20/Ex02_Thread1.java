2984f7a48ceca3b00e5bc62b485470f7d6c850bf
hyejin960622 <ch5335@gmail.com>
2022-05-24 09:32:21 +0900

    자바 20일차 : Thread 생성 예제 및 우선 순위 예제

---
+++ java/src/day20/Ex02_Thread1.java
@@ -0,0 +1,59 @@
+package day20;
+
+public class Ex02_Thread1 {
+
+	public static void main(String[] args) {
+		
+		//Test1 t1 = new Test1();
+		//Thread th1 = new Thread(t1);
+		Thread th1= new Thread(new Test1());
+		th1.setPriority(Thread.MAX_PRIORITY);
+		th1.start();
+		
+		Thread th2 = new Test2();
+		th2.setPriority(Thread.MIN_PRIORITY);
+		th2.start();
+		
+		Thread th3 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for(int i =0; i <10000; i++) {
+					System.out.println("@");
+				}
+				
+			}
+			
+		});
+		
+		th3.start();
+		for(int i =0; i <10000; i++) {
+			System.out.println("|");
+		}
+		
+
+	}
+
+}
+//Runnable 인터페이스를 구현한 구현 클래스 이용
+class Test1 implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i =0; i<100; i++) {
+			System.out.println("-");
+		}
+	}
+	
+}
+//Thread
+class Test2 extends Thread{
+	
+	@Override
+	public void run() {
+		for(int i =0; i <10000; i++) {
+			System.out.println("*");
+		}
+	}
+	
+}
