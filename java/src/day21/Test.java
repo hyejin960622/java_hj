c8ef5d3f821e3552efe03afe5b8f9f7cdcd88b46
hyejin960622 <ch5335@gmail.com>
2022-05-25 09:52:13 +0900

    자바 시험2

---
+++ java/src/day21/Test.java
@@ -0,0 +1,14 @@
+package day21;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		int sum = 0, i;
+		for(i =1; i<=10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+			
+	}
+
+}
